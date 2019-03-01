de367d345c23f9552927197a88b640d145096645
brennanmrose <brennan.m.rose@gmail.com>
2019-03-01 08:03:37 -0500

    update signup/login flow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,4 +13,40 @@ class UsersController < ApplicationController
 		end
 	end
 
+	post '/signup' do 
+		if params[:username].empty? || params[:email].empty? || params[:password].empty?
+			redirect '/failure'
+		else 
+			@user = User.create(username: params[:username], email: params[:email], password: params[:password])
+			redirect '/login'
+		end
+	end
+
+	get '/login' do 
+		if logged_in?
+			redirect :'users/show'
+		else
+			erb :'users/login'
+		end
+	end
+
+	post '/login' do
+		@user = User.find_by(username: params[:username])
+		if @user && @user.authenticate(params[:password])
+			session[:user_id] = @user.id 
+			redirect "/users/#{@user.slug}"
+		else
+			redirect '/signup'
+		end
+	end
+
+	get '/logout' do 
+		if logged_in?
+			session.clear
+			redirect '/login'
+		else
+			redirect '/'
+		end
+	end
+
 end
