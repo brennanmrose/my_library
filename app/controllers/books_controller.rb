0fbf9c41348880ae9475061ac73260ad8552b55b
brennanmrose <brennan.m.rose@gmail.com>
2019-03-01 08:57:31 -0500

    update post books method

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,7 +9,18 @@ class BooksController < ApplicationController
 	end
 
 	post '/books' do 
-		binding.pry
+		if params[:book][:title].present? && params[:author][:first_name].present? && params[:author][:last_name].present? && params[:genre].present?
+			@author = Author.create(first_name: params[:author][:first_name], last_name: params[:author][:last_name])
+			@book = Book.new(title: params[:book][:title], author: @author)
+			@book.genres.build(name: params[:genre])
+			@book.save
+			current_user.books << @book
+
+			redirect "/users/#{current_user.slug}"
+		else
+			# add flash error message
+			redirect '/books/new'
+		end
 	end
 
 end
