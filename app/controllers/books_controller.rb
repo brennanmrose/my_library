ec98d6f2e31e173dfa17a7012cf09d44ba938cfb
brennanmrose <brennan.m.rose@gmail.com>
2019-03-01 08:18:35 -0500

    create new book form and controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,3 +1,11 @@
 class BooksController < ApplicationController
 
+	get '/books/new' do 
+		if logged_in?
+			erb :'books/new'
+		else
+			redirect '/login'
+		end
+	end
+
 end
--- config.ru
+++ config.ru
@@ -6,5 +6,5 @@ end
 
 use Rack::MethodOverride
 use UsersController
-user BooksController
+use BooksController
 run ApplicationController
