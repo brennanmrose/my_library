9950db08479a72a05b26a111e12169e76f6480f5
brennanmrose <brennan.m.rose@gmail.com>
2019-03-02 21:00:55 -0500

    update post new book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,11 +9,15 @@ class BooksController < ApplicationController
 	end
 
 	post '/books' do 
-		if params[:book][:title].present? && params[:author][:first_name].present? && params[:author][:last_name].present? && params[:genre].present?
-			@author = Author.create(first_name: params[:author][:first_name], last_name: params[:author][:last_name])
-			@book = Book.new(title: params[:book][:title], author: @author)
-			@book.genres.build(name: params[:genre])
+		if valid_book_params?
+			@author = Author.create(author_params)
+			@book = Book.new(book_params)
+			@book.author = @author
 			@book.save
+			if (params[:genre][:name])
+				@genre = Genre.new(genre_params)
+				@book.genres << @genre
+			end
 			current_user.books << @book
 
 			redirect "/users/#{current_user.slug}"
@@ -23,4 +27,61 @@ class BooksController < ApplicationController
 		end
 	end
 
+	def book_params
+		params[:book]
+	end
+
+	def valid_book_params?
+		params[:book][:title].present? && params[:author][:first_name].present? && params[:author][:last_name].present? && params[:genre].present?
+	end
+
+	def author_params
+		params[:author]
+	end
+
+	def genre_params
+		params[:genre]
+	end
 end
+
+# class BooksController < ApplicationController
+
+# 	get '/books/new' do 
+# 		if logged_in?
+# 			erb :'books/new'
+# 		else
+# 			redirect '/login'
+# 		end
+# 	end
+
+# 	post '/books' do 
+# 			binding.pry
+# 		if valid_book_params?
+# 			@author = Author.create(author_params)
+# 			@book = Book.new(book_params)
+# 			@book.author = @author
+# 			# @book.genres.build(name: params[:genre])
+# 			@book.save
+# 			current_user.books << @book
+
+# 			redirect "/books/#{@book.slug}"
+# 		else
+# 			# add flash error message
+# 			redirect '/books/new'
+# 		end
+# 	end
+
+# 	get '/books/:slug' do 
+# 		if logged_in?
+# 			@book = Book.find(params[:id])
+# 			erb :'/books/show'
+# 		else
+# 			redirect '/login'
+# 		end
+# 	end
+
+# 	private
+
+
+
+# end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,8 @@ class UsersController < ApplicationController
 
 	post '/signup' do 
 		if params[:username].empty? || params[:email].empty? || params[:password].empty?
-			redirect '/failure'
+			# add flash error message
+			redirect '/signup'
 		else 
 			@user = User.create(username: params[:username], email: params[:email], password: params[:password])
 			redirect '/login'
