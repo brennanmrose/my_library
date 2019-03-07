a5b68e85caabe0e7710c194dc98ddb83a7c10c9e
brennanmrose <brennan.m.rose@gmail.com>
2019-03-06 21:40:03 -0500

    update login method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,8 +44,8 @@ class UsersController < ApplicationController
 	end
 
 	post '/login' do
-		@user = User.find_by(username: params[:username])
-		if @user && @user.authenticate(params[:password])
+		@user = User.find_by(username: params[:user][:username])
+		if @user && @user.authenticate(params[:user][:password])
 			session[:user_id] = @user.id 
 			redirect "/users/#{@user.slug}"
 		else
