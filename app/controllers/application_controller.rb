07abd5750d6843de3fa542ce886d95c42b2ca956
brennanmrose <brennan.m.rose@gmail.com>
2019-03-08 08:11:41 -0500

    fix sinatra flash config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,16 @@
 require './config/environment'
-# require 'sinatra/base'
-# require 'sinatra/flash'
+require 'sinatra/base'
+require 'sinatra/flash'
 
 class ApplicationController < Sinatra::Base
 
+  register Sinatra::Flash
+  
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
     set :session_secret, 'password_security'
-    # register Sinatra::Flash
   end
 
   get '/' do
